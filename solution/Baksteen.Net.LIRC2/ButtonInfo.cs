a7ffc25ffa28d14849726273d1dea62f56ed9093
jpmikkers <jean.paul.mikkers@gmail.com>
2024-06-23 19:14:55 +0200

    temp copy

---
+++ solution/Baksteen.Net.LIRC2/ButtonInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Baksteen.Net.LIRC;
+
+public record class ButtonInfo
+{
+    public string Button { get; init; } = String.Empty;
+    public string RemoteControl { get; init; } = String.Empty;
+    public byte[] Code { get; init; } = [];
+}
---
+++ solution/Baksteen.Net.LIRC2/DecodedButton.cs
@@ -0,0 +1,7 @@
+﻿namespace Baksteen.Net.LIRC;
+
+public record class DecodedButton
+{
+    public ButtonInfo ButtonInfo { get; init; } = new();
+    public int Repeat { get; init; }
+}
