c30f64823f99b57a1af0475b7bc4c77218072116
jpmikkers <jean.paul.mikkers@gmail.com>
2024-07-02 00:59:38 +0200

    add svg to icon conversion script

---
+++ icon/convertico.bat
@@ -0,0 +1,2 @@
+.\imagemagick\magick.exe -density 384 -background none -define icon:auto-resize .\lirctest.svg ..\solution\LIRCTestUI\Assets\lirctest.ico
+.\imagemagick\magick.exe -size 256x256 -background none .\lirctest.svg ..\solution\LIRCTestUI\Assets\lirctest_256x256.png
